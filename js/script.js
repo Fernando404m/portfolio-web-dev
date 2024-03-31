8e62bda85208f217469d4e0e7dca777bb0b42df7
Fernando <159110705+Fernando404m@users.noreply.github.com>
2024-03-31 20:42:57 -0300

    atualizaçao 3 about me

--- js/script.js
+++ js/script.js
@@ -7,3 +7,26 @@ function menu() {
         dentro_menu.style.display = "block"
     }
 }
+// animação do sobre mim
+var div_mov = document.querySelector("#mov_about_me").style
+window.addEventListener("scroll",function() {
+    var limite = this.document.getElementById("about_me").getBoundingClientRect().top
+    if (window.scrollY >= limite && window.scrollY < limite + 275) {
+        div_mov.top = "30%"
+        div_mov.position = "fixed"
+        div_mov.display = "block"
+        div_mov.right = window.scrollY - limite - 250 + "px"
+    }else if (window.scrollY >= limite + 275 && window.scrollY < limite + 600) {
+        div_mov.top = "30%"
+        div_mov.position = "fixed"
+        div_mov.display = "block"
+        div_mov.right = "23px"
+    }else if (window.scrollY >= limite + 600) {
+        div_mov.top = 649 + "px"
+        div_mov.position = "absolute"
+        div_mov.display = "block"
+        div_mov.right = "23px"
+    } else {
+        div_mov.display = "none"
+    }
+})
