cada30249823b7bbc2b98945d48670ca80a36b29
Fernando <159110705+Fernando404m@users.noreply.github.com>
2024-03-31 03:46:23 -0300

    atualizaçao 2

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// barra de nav
+var dentro_menu = document.getElementById("dentro_menu")
+function menu() {
+    if (dentro_menu.style.display == "block") {
+        dentro_menu.style.display = "none"
+    } else {
+        dentro_menu.style.display = "block"
+    }
+}
