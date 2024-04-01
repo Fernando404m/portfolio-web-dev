e163fc9e00fa27345de512067831a9e96e277030
Fernando <159110705+Fernando404m@users.noreply.github.com>
2024-03-31 21:18:18 -0300

    aperfeiçoamento da animaçao

--- js/script.js
+++ js/script.js
@@ -11,21 +11,22 @@ function menu() {
 var div_mov = document.querySelector("#mov_about_me").style
 window.addEventListener("scroll",function() {
     var limite = this.document.getElementById("about_me").getBoundingClientRect().top
+    var acrescimo = window.innerHeight - 568
     if (window.scrollY >= limite && window.scrollY < limite + 275) {
         div_mov.top = "30%"
         div_mov.position = "fixed"
         div_mov.display = "block"
-        div_mov.right = window.scrollY - limite - 250 + "px"
+        div_mov.right = (window.scrollY - limite) * (window.innerWidth/310) -  (window.innerWidth/100*80) + "px"
     }else if (window.scrollY >= limite + 275 && window.scrollY < limite + 600) {
         div_mov.top = "30%"
         div_mov.position = "fixed"
         div_mov.display = "block"
-        div_mov.right = "23px"
+        div_mov.right = "8%"
     }else if (window.scrollY >= limite + 600) {
-        div_mov.top = 649 + "px"
+        div_mov.top = 649 + acrescimo/3 + "px"
         div_mov.position = "absolute"
         div_mov.display = "block"
-        div_mov.right = "23px"
+        div_mov.right = "8%"
     } else {
         div_mov.display = "none"
     }
