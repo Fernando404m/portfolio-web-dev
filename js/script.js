baa188b94ae299391c3d4fd13db1c3ecae9ce295
Fernando <159110705+Fernando404m@users.noreply.github.com>
2024-04-14 18:52:44 -0300

    responsividade com js

--- js/script.js
+++ js/script.js
@@ -185,72 +185,81 @@ var barra_divisao = document.getElementById("divisoria_perfil")
 var posi_animaçao = 50
 
 function animação_perfil() {
-    if (posi_animaçao <= 300) {
-        
-        if (posi_animaçao >= 0 && posi_animaçao <= 100) {
-            perfil_html.style.top = posi_animaçao + "px"
-            perfil_css.style.top = 100 + posi_animaçao + "px"
-            perfil_js.style.top = 200 + posi_animaçao +"px"
-            perfil_js.style.zIndex = 1
-            perfil_css.style.zIndex = 2
-            perfil_html.style.zIndex = 0
-        }else if (posi_animaçao > 100 && posi_animaçao <= 200) {
-            perfil_html.style.top = posi_animaçao + "px"
-            perfil_css.style.top = 100 + posi_animaçao + "px"
-            perfil_js.style.top = -100 + posi_animaçao +"px"
-            perfil_js.style.zIndex = 0
-            perfil_css.style.zIndex = 1
-            perfil_html.style.zIndex = 2
-        }else if (posi_animaçao > 200) {
-            perfil_html.style.top = posi_animaçao + "px"
-            perfil_css.style.top = posi_animaçao - 200 + "px"
-            perfil_js.style.top = posi_animaçao - 100 +"px"
-            perfil_js.style.zIndex = 2
-            perfil_css.style.zIndex = 0
-            perfil_html.style.zIndex = 1
-        }
+    if (window.innerWidth >= 1260) {
+        if (posi_animaçao <= 300) {
+            
+            if (posi_animaçao >= 0 && posi_animaçao <= 100) {
+                perfil_html.style.top = posi_animaçao + "px"
+                perfil_css.style.top = 100 + posi_animaçao + "px"
+                perfil_js.style.top = 200 + posi_animaçao +"px"
+                perfil_js.style.zIndex = 1
+                perfil_css.style.zIndex = 2
+                perfil_html.style.zIndex = 0
+            }else if (posi_animaçao > 100 && posi_animaçao <= 200) {
+                perfil_html.style.top = posi_animaçao + "px"
+                perfil_css.style.top = 100 + posi_animaçao + "px"
+                perfil_js.style.top = -100 + posi_animaçao +"px"
+                perfil_js.style.zIndex = 0
+                perfil_css.style.zIndex = 1
+                perfil_html.style.zIndex = 2
+            }else if (posi_animaçao > 200) {
+                perfil_html.style.top = posi_animaçao + "px"
+                perfil_css.style.top = posi_animaçao - 200 + "px"
+                perfil_js.style.top = posi_animaçao - 100 +"px"
+                perfil_js.style.zIndex = 2
+                perfil_css.style.zIndex = 0
+                perfil_html.style.zIndex = 1
+            }
 
-        if (posi_animaçao > 90 && posi_animaçao < 210) {
-            perfil_js.style.opacity = 0
-            perfil_css.style.opacity = 0
-            perfil_html.style.opacity = 1
-            barra_divisao.style.backgroundImage = "linear-gradient(90deg, rgba(34, 34, 34, 0.575), rgba(85, 44, 44, 0.356))"
-        }else if (posi_animaçao > 190 || posi_animaçao < 10) {
-            perfil_js.style.opacity = 1
-            perfil_css.style.opacity = 0
-            perfil_html.style.opacity = 0
-            barra_divisao.style.backgroundImage = "linear-gradient(90deg, rgba(34, 34, 34, 0.575), rgba(85, 82, 44, 0.356))"
-        }else if (posi_animaçao > 290 || posi_animaçao < 110){
-            perfil_js.style.opacity = 0
-            perfil_css.style.opacity = 1
-            perfil_html.style.opacity = 0
-            barra_divisao.style.backgroundImage = "linear-gradient(90deg, rgba(34, 34, 34, 0.575), rgba(44, 66, 85, 0.356))"
+            if (posi_animaçao > 90 && posi_animaçao < 210) {
+                perfil_js.style.opacity = 0
+                perfil_css.style.opacity = 0
+                perfil_html.style.opacity = 1
+                barra_divisao.style.backgroundImage = "linear-gradient(90deg, rgba(34, 34, 34, 0.575), rgba(85, 44, 44, 0.356))"
+            }else if (posi_animaçao > 190 || posi_animaçao < 10) {
+                perfil_js.style.opacity = 1
+                perfil_css.style.opacity = 0
+                perfil_html.style.opacity = 0
+                barra_divisao.style.backgroundImage = "linear-gradient(90deg, rgba(34, 34, 34, 0.575), rgba(85, 82, 44, 0.356))"
+            }else if (posi_animaçao > 290 || posi_animaçao < 110){
+                perfil_js.style.opacity = 0
+                perfil_css.style.opacity = 1
+                perfil_html.style.opacity = 0
+                barra_divisao.style.backgroundImage = "linear-gradient(90deg, rgba(34, 34, 34, 0.575), rgba(44, 66, 85, 0.356))"
+            }
+            posi_animaçao += 5
+            setTimeout(function() {animação_perfil()}, 150)
+        }else {
+            posi_animaçao = 0
+            setTimeout(function() {animação_perfil()}, 150)
         }
-        posi_animaçao += 5
-        setTimeout(function() {animação_perfil()}, 150)
-    }else {
-        posi_animaçao = 0
-        setTimeout(function() {animação_perfil()}, 150)
     }
 }
-animação_perfil()
 
 
 // animaçao das estrelas do background do about me
 var posi_bg = 0
 var bgs = document.querySelectorAll(".estrela")
 function estrelas() {
-    if (window.scrollY > 50 && window.scrollY < 1800) {
-        if (posi_bg < 1000) {
-        bgs.forEach(function(bg) {bg.style.backgroundPosition = `${posi_bg}px ${posi_bg}px`})
-        posi_bg += 1
-        setTimeout(function() {estrelas()}, 200)
+    if (window.innerWidth >= 1260) {
+        if (window.scrollY > 50 && window.scrollY < 1800) {
+            if (posi_bg < 1000) {
+            bgs.forEach(function(bg) {bg.style.backgroundPosition = `${posi_bg}px ${posi_bg}px`})
+            posi_bg += 1
+            setTimeout(function() {estrelas()}, 200)
+            }else {
+            posi_bg = 0
+            setTimeout(function() {estrelas()}, 200)
+            }
         }else {
-        posi_bg = 0
-        setTimeout(function() {estrelas()}, 200)
+            setTimeout(function() {estrelas()}, 200)
         }
-    }else {
-        setTimeout(function() {estrelas()}, 200)
     }
 }
-estrelas()
+
+// executar
+if (window.innerWidth >= 1260) {
+    animação_perfil()
+    estrelas()
+}
+window.addEventListener("resize",function() {animação_perfil(); estrelas()})
